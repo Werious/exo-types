0acd7a4f4a88186eb0501e396c45ee9c0b4126d3
Werious <51258195+Werious@users.noreply.github.com>
2023-09-22 16:00:32 +0200

    Add files via upload

---
+++ types.js
@@ -0,0 +1,21 @@
+let avengers = 7; //number
+let nomDeFamille = "Cesar"; //string
+let etoile = 500000000; //number
+let message = "Bienvenue" //string
+let faceId = true; //booleen
+let prix = 2500; //number
+let septNains = ["Pierre", "Paul", "Jack"]; //object
+let tinder = {
+    "nom" : "Jean",
+    "age" : "25",
+    "phraseDeMotivation" : "Je suis motivé",
+    "hobby" : ["sport","lecture","entreprenariat"],
+}; //literal object
+let clones = 17; //number
+let defauts = ["premier", "deuxième", "troisième"]; // object
+let anne = "Anne"; //string
+let panier = ["permier achat", "deuxième achat", "troisième achat"]; //object
+let guerre = "plus de 100 ans"; //string
+let bière = ["brune","blonde","rousse"]; //object
+let prochainPresident = "le président avant le président du quinquenat suivant"; //string
+
